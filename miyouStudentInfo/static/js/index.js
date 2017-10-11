970e9c3053b651fc84b2e499804d13064db728a3
zhuoheng <heng.zhuo@juphoon.com.cn>
2017-10-11 17:49:53 +0800

    修复认证列表

--- miyouStudentInfo/static/js/index.js
+++ miyouStudentInfo/static/js/index.js
@@ -6,9 +6,9 @@ function ajax(dataObj){
 		layer.load()
 
 	})
-    // var url = 'http://192.168.12.1:8086/miyou/'
+    var url = 'http://192.168.12.1:8086/miyou/'
     // var url = 'http://192.168.15.161:8086/miyou/'
-    var url = 'http://218.204.254.209:28811/miyou/'
+    // var url = 'http://218.204.254.209:28811/miyou/'
 
 
     url += dataObj.url
