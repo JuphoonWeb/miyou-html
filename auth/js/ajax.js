ca601e230c504e70208579930ac176256ec7ef28
Philip <494129479@qq.com>
2017-11-10 10:20:55 +0800

    feature: 添加新APP认证界面

---
+++ auth/js/ajax.js
@@ -0,0 +1,47 @@
+
+function ajax(obj){
+
+
+	var url = 'http://122.227.209.194:8086/miyou/api/'
+	// var url = 'http://192.168.0.46:8086/miyou/api/'
+
+    // var url = 'http://218.204.254.209:28811/miyou/api/' 
+
+    url += obj.url
+	
+
+	var successFunc = function(res){
+	    	console.log('success',res)
+	    	obj.success && obj.success(res)
+	    },
+	    errorFunc = function(xhr, status){
+	    	console.log('error',status)
+	    	obj.error && obj.error(xhr, status)
+	    },
+	    beforeSendFunc = function(xhr, settings){
+	    	console.log('beforesend')
+	    	obj.beforeSend && obj.beforeSend(xhr, settings)
+	    }
+
+
+	$.ajax({
+		
+		url: url,
+		type: obj.type,
+		headers: {
+			token: localStorage.getItem('api_token')
+		},
+		async: obj.async,
+		timeout: obj.timeout || 5*1000,
+		cache: obj.cache,
+		processData: obj.processData,
+	    contentType: obj.contentType,
+		dataType : 'json',
+		data: obj.data,
+		timeout: obj.timeout,
+		beforeSend: beforeSendFunc,
+		success: successFunc,
+		error: errorFunc 
+	})
+}
+
---
+++ auth/js/index.js
@@ -0,0 +1,71 @@
+//dialog插件封装
+
+function toast(text, status){
+	var iconUrl = ''
+	if(status === 'success'){
+		iconUrl = '../img/success.png'
+	}else if(status === 'fail'){
+		iconUrl = '../img/fail.png'
+	}else if(status === 'loading'){
+		iconUrl = '../img/loading.gif'
+	}
+	$(document).dialog({
+		type: 'toast',
+		infoIcon: iconUrl,
+		infoText: text,
+		autoClose: 1500
+	})
+}
+function dialog(obj){
+	 var dialog = $(document).dialog({
+        type : obj.type || 'alert',
+        style: 'default',  // default、ios、android
+        // titleText: title,
+        content: obj.text,
+        onClickConfirmBtn: obj.ok
+    });
+	return dialog
+}
+function notice(text){
+	$(document).dialog({
+        type : 'notice',
+        infoText: text,
+        autoClose: 1500
+    });
+}
+
+
+ //根据请求url获取参数
+function GetQueryString(name){
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var currUrl = window.location.search
+     var r = currUrl.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
+
+// Date对象格式化
+Date.prototype.Format = function (fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+}
+
+
+function isEmpty(obj)
+{
+    for (var name in obj) 
+    {
+        return false;
+    }
+    return true;
+};
